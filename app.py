fa550ac2b8c778fcf5aac242c9fc54eb62d39c02
Rakesh Chinthakrindi <rac@microsoft.com>
2019-12-19 14:13:18 +0530

    Summary flask app

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restful import Resource, reqparse, Api
+from gensim.summarization import summarize
+
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class Summary(Resource):
+	parser = reqparse.RequestParser()
+	parser.add_argument('Text', type=str, required=True, help='Conversation text')
+	parser.add_argument('Ratio', type=float, required=False, help='Summary ratio')
+
+	def get(self):
+		args = Summary.parser.parse_args()
+		text =  args['Text']
+		ratio = 0.7
+		if args['Ratio']:
+			ratio = args['Ratio']
+		summarized_text = summarize(text, ratio)
+		return {'Topic': summarized_text.split(".")[0], 'Summmary': summarized_text.replace('\n',' ')}
+
+api.add_resource(Summary, '/summary')
+
+if __name__=='__main__':
+    app.run(debug=True)
